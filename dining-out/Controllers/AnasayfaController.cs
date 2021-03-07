5384010ffc2bae0874f0f06c8dc920fe80ad7598
Cigdem Imre <cigdem.imrem@gmail.com>
2021-03-07 15:50:08 +0300

    İlk boş uygulama oluşturuldu. Ana sayfa ekrana geldiği görüldü.

---
+++ dining-out/Controllers/AnasayfaController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using dining_out.Models.ViewModels;
+
+namespace dining_out.Controllers
+{
+    public class AnasayfaController : Controller
+    {
+        private readonly ILogger<AnasayfaController> _logger;
+
+        public AnasayfaController(ILogger<AnasayfaController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Hata()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ dining-out/Models/ViewModels/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace dining_out.Models.ViewModels
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
