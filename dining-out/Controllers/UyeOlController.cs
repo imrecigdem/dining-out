ec0adfe702a3ff32dbbbf954073a100374186e16
Cigdem Imre <cigdem.imrem@gmail.com>
2021-03-14 20:56:21 +0300

    UyeOl Sayfası ve Controller sınıfı basit olarak eklendi.

---
+++ dining-out/Controllers/UyeOlController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using dining_out.Models.ViewModels;
+using dining_out.Models.DbModels;
+using dining_out.Utility;
+
+namespace dining_out.Controllers
+{
+    public class UyeOlController : Controller
+    {
+        private readonly ILogger<UyeOlController> _logger;
+
+        public UyeOlController(ILogger<UyeOlController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IActionResult Index(int restaurantId)
+        {
+            return View("Index");
+        }
+
+
+
+    }
+}
