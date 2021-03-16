89e9ef4f837f80c409d00398510f397725bb75d4
Gulsah Erturk <Gulsaherturk>
2021-03-16 14:03:44 +0300

    Login sonrası kullanıcıların işlemlerini yapabilmesi için Dashboard Anasayfası ortak layout olarak eklendi.

---
+++ dining-out/Controllers/DashboardAnasayfaController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using dining_out.Models.ViewModels;
+using dining_out.Models.DbModels;
+
+namespace dining_out.Controllers
+{
+    public class DashboardAnasayfaController : Controller
+    {
+        private readonly ILogger<DashboardAnasayfaController> _logger;
+
+        public DashboardAnasayfaController(ILogger<DashboardAnasayfaController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Hata()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
