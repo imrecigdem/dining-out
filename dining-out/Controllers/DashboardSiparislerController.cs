ffa1692529506b8341544e22c15e1926e13d15bb
Cigdem Imre <imrecigdem>
2021-04-03 17:42:20 +0300

    Rezervasyon üzerinden alınan siparişler sayfası restaruantlar için draft olarak eklenmiştir.

---
+++ dining-out/Controllers/DashboardSiparislerController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using dining_out.Models.ViewModels;
+using dining_out.Models.DbModels;
+using dining_out.Utility;
+
+namespace dining_out.Controllers
+{
+    public class DashboardSiparislerController : Controller
+    {
+        private readonly ILogger<DashboardSiparislerController> _logger;
+
+        public DashboardSiparislerController(ILogger<DashboardSiparislerController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            int userId=1; // Login Kullanıcı olacak
+            diningoutContext dbContext = new diningoutContext();
+            User user = dbContext.Users.Where(user => user.Id.Equals(userId)).SingleOrDefault();
+
+
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Hata()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
